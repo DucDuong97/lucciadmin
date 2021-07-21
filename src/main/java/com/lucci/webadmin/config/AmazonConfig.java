0f9b7bb57551bf3d9e786966fcb1927e6a114a9d
DUCDUONG <duong.tracer@gmail.com>
2021-07-22 02:35:05 +0700

    (env) (FileStore): move s3 configuration to app properties

--- src/main/java/com/lucci/webadmin/config/AmazonConfig.java
+++ src/main/java/com/lucci/webadmin/config/AmazonConfig.java
@@ -5,18 +5,27 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
+
+    @Value("${amazon.s3.credential.accessKey}")
+    private String accessKey;
+    @Value("${amazon.s3.credential.secretKey}")
+    private String secretKey;
+    @Value("${amazon.s3.region}")
+    private String region;
+
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials =
-            new BasicAWSCredentials("AKIAU4NWC3CKMDBOVHFY", "U1lQvR898Zp0DllbQT4d8TNZJxuMHjEO8bUDhJsE");
+            new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder
             .standard()
-            .withRegion("ap-southeast-1")
+            .withRegion(region)
             .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
             .build();
 
